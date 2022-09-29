85dcd38b4fc36975c9fa34fd6768c1a285dd5485
Prajakta G. Girulkar <Prajakta G. Girulkar@DESKTOP-Q3EFVGA>
2022-09-28 17:37:19 -0700

    Spring MVC Addition project using Java based Configuration

---
+++ SpringMVC-Add-JBC/src/main/java/com/demo/Add/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.demo.Add;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.demo.Add")
+public class MvcConfig  extends WebMvcConfigurerAdapter{
+
+
+}
---
+++ SpringMVC-Add-JBC/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+	<h2>Hello !</h2>
+	
+		<h2>Add Two Numbers !!!!</h2>
+	<form action="add">
+		<input type="Number1" name="t1"><br> <input type="Number2"
+			name="t2"><br> <input type="submit"><br>
+	</form>
+</body>
+</html>
